544e812ae69697cc7c1a01ff926d7983838e8640
Marian Metsch <59338660+engine20@users.noreply.github.com>
2022-07-22 20:48:58 +0200

    Finished display

--- Display/Display.lua
+++ Display/Display.lua
@@ -11,7 +11,9 @@ local delay = require('modules/delay')
 
 _CFG = {maxpopups = 10}
 local carstate = {
-  page = 0 -- defines the first page upon loading
+  page = 0, -- defines the first page upon loading
+  maxstintfuel = 0,
+  fuelPerLap = 0
 }
 INF = 1e308
 Legacy = ac.getPatchVersionCode() <= 1709
@@ -42,6 +44,16 @@ eventListener:new(function() return simObject.raceSessionType; end, function(_, 
   carstate.page = 0;
 end)
 
+eventListener:new(function() return car.fuel; end, function(diff)
+  pcall(function()
+    if diff > 0 or (Legacy and (car.isInPitlane and car.speedKmh < 1) or car.isInPit) then
+      carstate.maxstintfuel = car.fuel;
+    end
+  end)
+end)
+
+eventListener:new(function() return car.lapCount; end,
+                  function(_, newVal) carstate.fuelPerLap = (carstate.maxstintfuel - car.fuel) / newVal; end)
 -- #region
 -- Popups
 
@@ -62,13 +74,10 @@ function update(dt)
   ---@diagnostic disable-next-line: deprecated
   simObject = Legacy and ac.getSimState() or ac.getSim();
 
+  if G.firstrun then carstate.maxstintfuel = car.fuel; end
+
   if carstate.page == 0 then
     display.image({image = 'Display/texDisp.zip::R1.dds', pos = vec2(0, 0), size = vec2(1024, 512)})
-    Utils.displayText(Utils.dec(car.oilTemperature, 1, '.'), 328, 137, 0.9, '488', {1, 1, 1}, 'center')
-    Utils.displayText(Utils.dec(car.oilPressure, 1, '.'), 328, 184, 0.9, '488', {1, 1, 1}, 'center')
-    Utils.displayText(Utils.dec(car.waterTemperature, 1, '.'), 328, 231, 0.9, '488', {1, 1, 1}, 'center')
-    Utils.displayText(Utils.dec((car.waterTemperature - 20) ^ 0.3 * car.oilPressure / 5, 1, '.'), 328, 278, 0.9, '488',
-                      {1, 1, 1}, 'center') -- Water Pressure
   elseif carstate.page == 1 then
     display.image({image = 'Display/texDisp.zip::Q.dds', pos = vec2(0, 0), size = vec2(1024, 512)})
   elseif carstate.page == 2 then
@@ -94,6 +103,29 @@ function update(dt)
       Utils.displayText((car.performanceMeter == 0 and '  ' or (car.performanceMeter >= 0 and '+ ' or '- ')) ..
                           Utils.dec(math.abs(math.clampN(car.performanceMeter, -99, 99)), 2, '.'), 691, 271, 0.9, '488',
                         {1, 1, 1}, 'center')
+      if carstate.page == 0 then
+        Utils.displayText(Utils.dec(car.oilTemperature, 1, '.'), 328, 137, 0.9, '488', {1, 1, 1}, 'center')
+        Utils.displayText(Utils.dec(car.oilPressure, 1, '.'), 328, 184, 0.9, '488', {1, 1, 1}, 'center')
+        Utils.displayText(Utils.dec(car.waterTemperature, 1, '.'), 328, 231, 0.9, '488', {1, 1, 1}, 'center')
+        Utils.displayText(Utils.dec((car.waterTemperature - 20) ^ 0.3 * car.oilPressure / 5, 1, '.'), 328, 278, 0.9,
+                          '488', {1, 1, 1}, 'center') -- Water Pressure
+      else
+        Utils.displayText(Utils.dec(carstate.maxstintfuel - car.fuel, 1, ','), 328, 137, 0.9, '488', {1, 1, 1}, 'center')
+        Utils.displayText(Utils.dec(carstate.fuelPerLap, 1, ','), 328, 184, 0.9, '488', {1, 1, 1}, 'center')
+        Utils.displayText('80.00', 328, 231, 0.9, '488', {1, 1, 1}, 'center')
+        Utils.displayText(Utils.dec(car.fuel, 1, '.'), 328, 278, 0.9, '488', {1, 1, 1}, 'center')
+      end
+    else
+      Utils.displayText(Utils.timeformat(car.lapTimeMs, {':', '.'}, 2, true), 263, 223, 1.6, '488', {1, 1, 1}, 'center')
+      Utils.displayText((car.performanceMeter == 0 and '  ' or (car.performanceMeter >= 0 and '+ ' or '- ')) ..
+                          Utils.dec(math.abs(math.clampN(car.performanceMeter, -99, 99)), 3, '.'), 751, 195, 1.4, '488',
+                        {1, 1, 1}, 'center')
+      display.image({image = 'Display/texDisp.zip::Overlay.png', pos = vec2(634, 253), size = vec2(281, 65)})
+      display.rect({
+        pos = vec2(775, 254),
+        size = vec2(140 * math.clampN(car.performanceMeter / 0.9, -1, 1), 65),
+        color = rgb(1, 1, 1)
+      })
     end
     Utils.displayText(car.gear > 0 and car.gear or (car.gear == 0 and 'N' or 'R'), 512, 153, 4.0, '488', {1, 1, 1},
                       'center')
